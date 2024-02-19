4d6aa5f436ab402f65a38aadb833cd3725675ade
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-02-19 06:03:10 -0500

    fetched 9 pokemon from API and stored as objects

--- pokemon-memory-card-game/src/Components/Body.jsx
+++ pokemon-memory-card-game/src/Components/Body.jsx
@@ -34,13 +34,61 @@ function Body(){
                 }));
                 setPokemonList(p=>[...pokemonList,{pokemon}]); // 2nd pokemon
 
-                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/salamander")
+                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/salamence")
                 data=await(apiReturn.json());
                 setPokemon(p=>({
                     name:data.name,
                     image:data.sprites.shiny,
                 }));
                 setPokemonList(p=>[...pokemonList,{pokemon}]); // 3rd pokemon
+
+                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/gengar")
+                data=await(apiReturn.json());
+                setPokemon(p=>({
+                    name:data.name,
+                    image:data.sprites.shiny,
+                }));
+                setPokemonList(p=>[...pokemonList,{pokemon}]); // 4th pokemon
+
+                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/garchomp")
+                data=await(apiReturn.json());
+                setPokemon(p=>({
+                    name:data.name,
+                    image:data.sprites.shiny,
+                }));
+                setPokemonList(p=>[...pokemonList,{pokemon}]); // 5th pokemon
+
+                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/kyurem")
+                data=await(apiReturn.json());
+                setPokemon(p=>({
+                    name:data.name,
+                    image:data.sprites.shiny,
+                }));
+                setPokemonList(p=>[...pokemonList,{pokemon}]); // 6th pokemon
+
+                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/greninja")
+                data=await(apiReturn.json());
+                setPokemon(p=>({
+                    name:data.name,
+                    image:data.sprites.shiny,
+                }));
+                setPokemonList(p=>[...pokemonList,{pokemon}]); // 7th pokemon
+
+                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/gyarados")
+                data=await(apiReturn.json());
+                setPokemon(p=>({
+                    name:data.name,
+                    image:data.sprites.shiny,
+                }));
+                setPokemonList(p=>[...pokemonList,{pokemon}]); // 8th pokemon
+
+                apiReturn = await fetch("https://pokeapi.co/api/v2/pokemon/blaziken")
+                data=await(apiReturn.json());
+                setPokemon(p=>({
+                    name:data.name,
+                    image:data.sprites.shiny,
+                }));
+                setPokemonList(p=>[...pokemonList,{pokemon}]); //9th pokemon
 
 
             }
