b69086b4736bb56fa0b01a9671d2b1f33fff48aa
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-02-19 06:37:59 -0500

    randomize pokemon complete

--- pokemon-memory-card-game/src/Components/Body.jsx
+++ pokemon-memory-card-game/src/Components/Body.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 function Body() {
   const [pokemonList, setPokemonList] = useState([]);
   const [clickedPokemon,setClickedPokemon]=useState([]);
+  const [unique,setUnique]=useState(0);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -37,26 +38,42 @@ function Body() {
     fetchData();
   }, []);
 
+  function handleLoss(){
+    console.log("ran");
+  }
   function handlePokemonClick(name){
     // randomize the indexes of pokemon.
     console.log(name)
-    if(clickedPokemon.has(name)){
-        console.log("has!!");
-    }else{
-        setClickedPokemon(p=>[...clickedPokemon,name]);
+    for(let i=0;i<clickedPokemon.length+1;i++){
+        if(clickedPokemon[i]==name){
+            handleLoss();
+        }else{
+            setClickedPokemon(p=>[...clickedPokemon,name]);
+            
+            let shuffled = pokemonList
+                .map(value => ({ value, sort: Math.random() }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({ value }) => value)
+            
+            setPokemonList(p=>shuffled);
+        }
     }
-    
   }
 
+
   return (
-    <div className="pokemon-grid">
-      {pokemonList.map((poke, index) => (
-        <div key={index} className="grid-item">
-          <p>{poke.name}</p>
-          <img src={poke.image} alt={poke.name} onClick={()=>{handlePokemonClick(poke.name)}}/>
+    <div>
+        <h2 className="counter">Unique Pokemon Clicked: {unique}</h2>
+        <div className="pokemon-grid">
+            {pokemonList.map((poke, index) => (
+            <div key={index} className="grid-item">
+            <p>{poke.name}</p>
+            <img src={poke.image} alt={poke.name} onClick={()=>{handlePokemonClick(poke.name)}}/>
+            </div>
+            ))}
         </div>
-      ))}
     </div>
+    
   );
 }
 
